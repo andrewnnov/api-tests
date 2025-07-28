1e99b4837f8a374cf12cc60d76f7a155d448b8f7
andrewnnov <andrewnnov@yandex.ru>
2025-07-29 02:09:48 +0300

    add test

---
+++ src/main/java/specs/ResponseSpecs.java
@@ -0,0 +1,20 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+
+public class ResponseSpecs {
+    private ResponseSpecs() {}
+
+    private static ResponseSpecBuilder defaultResponseBuilder() {
+        return new ResponseSpecBuilder();
+    }
+
+    public static ResponseSpecification entityWasCreated() {
+        return defaultResponseBuilder()
+                .expectStatusCode(HttpStatus.SC_CREATED)
+                .build();
+    }
+
+}
